f7b4b7460257072685186d272ac9094ec672a4ae
Viona <viona.maryati@stockbit.com>
2022-07-01 05:51:27 +0700

    database url

--- src/main/kotlin/com/viona/onlinemotorcycle/database/DatabaseComponent.kt
+++ src/main/kotlin/com/viona/onlinemotorcycle/database/DatabaseComponent.kt
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Component
 
 @Component
 class DatabaseComponent {
-
-    //private val databaseUrl = System.getenv("DATABASE_URL")
-    private val databaseUrl3 = "mongodb+srv://vionavio:666198@cluster0.fybrt.mongodb.net/?retryWrites=true&w=majority"
-    val database: MongoClient = KMongo.createClient(databaseUrl3)
+    private val databaseUrl = System.getenv("DATABASE_URL")
+    val database: MongoClient = KMongo.createClient(databaseUrl)
 }
